c2246461f46d157ecf519a211ae739e1450ff838
ellanaqira <ellanrepo@gmail.com>
2026-06-01 09:19:36 +0800

    feat: add string_comparison function for command input and un-comment command input loop

--- name_list.c
+++ name_list.c
@@ -3,53 +3,61 @@
 #define MAXCHAR 1000
 
 /*function declaration*/
-void name_input(char list_of_name[]);
-char command_input(char command[]);
+void input_menu(char input[]);
 void separated_name(char from_combined[]);
+int string_comparison(char str1[], char str2[]);
 
 
 int main() {
     char list_of_name[MAXCHAR];
     char command[MAXCHAR];
+    char available_cmd[] = "'help' - to show all available commands\n'list' - print name line by line\n'quit' - ends the program\n";
+    // if (string_comparison("help\0", "help") == 1) {
+    //     printf("corect\n");
+    // }
 
+    // else {
+    //     printf("false\n");
+    // }
     printf("\n[NAME INPUT]");
     printf("\nInput list of name in one line separated by coma (,)\n");
     printf("example : 1st name,2nd name,....(ENTER)\n");
     printf("> ");
-    name_input(list_of_name);
+    input_menu(list_of_name);
     
     printf("\n[COMMAND INPUT]");
-    printf("\ntype 'help' to see available command\n");
-    printf("> ");
-    command_input(command);
-    printf("%s\n", command);
+    printf("\ntype 'help' to see available command");
+
+
+    while (1) {
+        printf("\n> ");
+        input_menu(command);
+
+        if (string_comparison(command, "quit") == 1) {
+            break;
+        }
+        else if (string_comparison (command, "help") == 1) {
+            printf("%s", available_cmd);
+        }
+        else if (string_comparison (command, "list") == 1) {
+            separated_name(list_of_name);
+            printf("%s\n", list_of_name);
+        }
+        else {
+            printf("commands not available\n");
+        }
+    }    
 
     return 0;
 }
 
-void name_input(char list_of_name[]) {
-    int name_list;
+void input_menu(char input[]) {
+    int keyboard_input;
     int i = 0;
-    while ((name_list = getchar()) != EOF) {
-        list_of_name[i] = name_list;
-
-        if (name_list == '\n') {
-            list_of_name[++i] = name_list = '\0';
-            break;
-        }
-        ++i;
-    }
-}
-
-
-char command_input(char command[]) {
-    int cmmd;
-    int i = 0;
-    while ((cmmd = getchar()) != EOF) {
-        command[i] = cmmd;
-        if (cmmd == '\n') {
-            command[i] = cmmd;
-            command[i] = '\0';
+    while ((keyboard_input = getchar()) != EOF) {
+        input[i] = keyboard_input;
+        if (keyboard_input == '\n') {
+            input[i] = '\0';
             break;
         }
         ++i;
@@ -63,8 +71,21 @@ void separated_name(char from_combined[]) {
 
     while (from_combined[i] != '\0') {
         if (from_combined[i] == ',') {
-            from_combined[i] = '=';
+            from_combined[i] = '\n';
         }
         ++i;
     }
+}
+
+
+int string_comparison(char str1[], char str2[]) {
+    int i = 0;
+
+    while (str1[i] == str2[i] ) {
+        if (str1[i] == '\0') {
+            return 1;
+        }
+        ++i;
+    }
+    return 0;
 }
