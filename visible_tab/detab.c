022010ab1e66d9a347736cc8ba646554c0a80076
ellanaqira <ellanrepo@gmail.com>
2026-06-06 18:13:05 +0800

    feat: make visible_tab/detap.c to turn tab into hyphen(-) according to the number of spaces on the tab

---
+++ visible_tab/detab.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+#define Space_Len 8
+// Function declaration
+void detab (int raw_input);
+
+
+int main (void) {
+    int c;
+    printf("\nThis program will change 'tab' into hyphen(-)\n");
+    printf("according to the number of spaces on the tab\n\n");
+    
+    detab(c);
+    return 0;
+}
+
+// change tab into '-' according to the ammount of blank from tab itself
+// to make tab more visible
+void detab (int raw_input) {
+    int i;
+
+    printf("> ");
+    while((raw_input = getchar()) != EOF) {
+        if (raw_input == '\t') {
+            for (i=0; i<=Space_Len; ++i) {
+                putchar('-');
+            }
+        }
+        else {
+            putchar(raw_input);
+        }
+    }
+}
+
+// This verry simple project is based from Excercise 1-20 from C Programming
+// Language Second Edition by Brian W.Kernighan & Dennis M.Ritchie. I made a
+// slight modification: instead of replace tap with propper number of blanks,
+// i replace it with propper number of hyphen(-).
