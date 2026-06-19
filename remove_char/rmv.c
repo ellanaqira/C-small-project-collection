21c0c5d7f385afb2c48d7b1765835305692a22fb
ellanaqira <ellanrepo@gmail.com>
2026-06-19 13:32:42 +0800

    chore: rename file to remove_char/rmv.c docs: add project description comment

---
+++ remove_char/rmv.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+// constant
+#define CHARMAX 1000
+
+// function declaration
+void rmchar(char input[], char stain);
+
+int main(void){
+    char input[CHARMAX] = "AaaaAAaaAaaaaA";
+    char stain = 'A';
+
+    printf("before: %s\n", input);
+    rmchar(input, stain);
+    printf("after : %s\n", input);
+    return 0;
+}
+
+void rmchar(char input[], char stain) {
+    int i;
+    int j = 0;
+    for (i=0; input[i] != '\0'; i++) {
+        if (input[i] != stain) {
+            input[j] = input[i];
+            j++;
+        }
+    }
+    input[j] = '\0';
+}
+
+// you can run it by execute this command: ./rmv.exe
+// This verry simple project is based from Excercise 2-4 from C Programming
+// Language Second Edition by Brian W.Kernighan & Dennis M.Ritchie. I add
+// little decorations like show the string before and after.
