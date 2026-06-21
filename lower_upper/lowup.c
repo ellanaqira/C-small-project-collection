3db6f316147190d682107360d0c582b18d238e79
ellanaqira <ellanrepo@gmail.com>
2026-06-21 15:54:44 +0800

    docs: add project description

--- lower_upper/lowup.c
+++ lower_upper/lowup.c
@@ -28,3 +28,8 @@ int upper(int c) {
         return c;
     }
 }
+
+// you can run it by execute this command: ./lowup.exe
+// This verry simple project is based from example function from C Programming
+// Language Second Edition by Brian W.Kernighan & Dennis M.Ritchie, Section 2.7,
+// page 43. I I made upper function too for more practice.
