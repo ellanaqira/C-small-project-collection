d7754650d4be0af0e478e1913820f37fe1d5112d
ellanaqira <ellanrepo@gmail.com>
2026-07-11 16:59:45 +0800

    docs: add code description at the bottom of file refator: change the argument at getbits function docs: remove unnecessary comments

--- C_small_projects/get_bits/getb.c
+++ C_small_projects/get_bits/getb.c
@@ -3,17 +3,27 @@
 // function declarartion
 unsigned getbits(unsigned x, int p, int n);
 
+
+// MAIN
 int main(void) {
 	unsigned x;
-	// 9 = 0000 1001 -> 0010 0100
-	// 0 = 0000 0000  ->  1111 1111 -> 0001 1111 -> 1110 0000
-	// 0010 0000
 
-	int num = getbits(9,4,3);
+	int num = getbits(29,4,3);
 	printf("%d\n", num);
 	return 0;
 }
 
+
+// Function Definition
 unsigned getbits(unsigned x, int p, int n) {
 	return (x >> (p+1-n)) & ~(~0 << n);
 }
+
+/*
+This is a code snippet from The C Programming Book
+by K&R page 49. To understand how this code work,
+check my project at bitwise_operators/bitw.c, run
+bitw.c and the it will print out the explanation for
+Bitwise Operator included with visual expalanation
+using ASCII Art.
+*/
