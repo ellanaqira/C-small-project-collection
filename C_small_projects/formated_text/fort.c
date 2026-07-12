522f5fe77686714117deec38f5be899271dfb4f4
ellanaqira <ellanrepo@gmail.com>
2026-07-12 17:31:31 +0800

    feat: add upper and lower function

--- C_small_projects/formated_text/fort.c
+++ C_small_projects/formated_text/fort.c
@@ -2,17 +2,25 @@
 
 // Function Declaration
 void titled(char *s);
+void upper(char *s);
+void lower(char *s);
 
 
 // MAIN
 int main(void) {
     int num = 0;
-    char s1[6] = "ellan";
-    char s2[6] = "Aqira";
+    char s1[] = "ellan";
+    char s2[] = "eLlaN";
+    char s3[] = "ELLAN";
 
     titled(s1);
+    upper(s2);
+    lower(s3);
+
     printf("%s\n", s1);
     printf("%s\n", s2);
+    printf("%s\n", s3);
+
     return 0;
 }
 
@@ -35,4 +43,32 @@ void titled(char *s) {
         }
         s[i] = '\0';
     }
+}
+
+
+void upper(char *s) {
+    int i = 0;
+    for (i=0; s[i] != '\0'; i+=1) {
+        if (s[i] >= 97 && s[i] <= 122) {
+            s[i] = s[i] - 32;
+        }
+        else {
+            s[i] = s[i];
+        }
+    }
+    s[i] = '\0';
+}
+
+
+void lower(char *s) {
+    int i = 0;
+    for (i=0; s[i] != '\0'; i+=1) {
+        if (s[i] >= 65 && s[i] <= 90) {
+            s[i] = s[i] + 32;
+        }
+        else {
+            s[i] = s[i];
+        }
+    }
+    s[i] = '\0';
 }
